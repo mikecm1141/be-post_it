86ac689eaa68b5459907bf4f7aa0a3450fe16d49
Mike <mikecm@gmail.com>
2019-03-02 18:16:26 -0700

    Add has_many relationship to Topics

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,3 +1,5 @@
 class Topic < ApplicationRecord
+  has_many :posts
+
   validates :title, :description, presence: true
 end
--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -5,4 +5,8 @@ RSpec.describe Topic, type: :model do
     it { should validate_presence_of :title }
     it { should validate_presence_of :description }
   end
+
+  describe 'Relationships' do
+    it { should have_many :posts }
+  end
 end
