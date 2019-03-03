c5acfeb4f021d801e80dfc02361d18d8ead51a9d
Mike <mikecm@gmail.com>
2019-03-02 18:14:14 -0700

    Create migration for Post table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_03_010634) do
+ActiveRecord::Schema.define(version: 2019_03_03_011128) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.bigint "topic_id"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["topic_id"], name: "index_posts_on_topic_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "topics", force: :cascade do |t|
     t.string "title"
@@ -33,4 +44,6 @@ ActiveRecord::Schema.define(version: 2019_03_03_010634) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "posts", "topics"
+  add_foreign_key "posts", "users"
 end
